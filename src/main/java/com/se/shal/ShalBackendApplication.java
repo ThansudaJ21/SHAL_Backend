5e64f369e75a629556e39d306ac7625be3d61f27
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2022-05-03 18:20:30 +0700

    init

---
+++ src/main/java/com/se/shal/ShalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.se.shal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se/shal/ShalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.shal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
