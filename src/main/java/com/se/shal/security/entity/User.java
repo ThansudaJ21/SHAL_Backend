d6c67745ba8fb89a992c9765d9aa2d921bbcb38e
Patteera <patteera_mu@cmu.ac.th>
2022-07-14 15:23:53 +0700

    add length of picture column

--- src/main/java/com/se/shal/security/entity/User.java
+++ src/main/java/com/se/shal/security/entity/User.java
@@ -36,7 +36,7 @@ public class User {
     @NotNull
     private String userId;
 
-    @Column(name = "PICTURE", length = 100)
+    @Column(name = "PICTURE", length = 256)
     @NotNull
     private String pictureUrl;
 
