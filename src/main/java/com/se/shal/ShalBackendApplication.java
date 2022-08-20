fdc5cda6c126ac85f40fac3638a6821fafe15bcd
Thansuda <thansuda_janthakham@cmu.ac.th>
2022-08-20 20:24:42 +0700

    add cors config

--- src/main/java/com/se/shal/ShalBackendApplication.java
+++ src/main/java/com/se/shal/ShalBackendApplication.java
@@ -25,7 +25,7 @@ public class ShalBackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://shal.loca.lt/")
+                        .allowedOrigins("http://localhost:3000", "https://shal.loca.lt/","https://shal-fdc49.web.app/")
                         .exposedHeaders("x-total-count");
             }
         };
