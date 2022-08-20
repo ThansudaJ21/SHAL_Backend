d05591926ecfb805c85e52d9ba1202c761afe0e7
Thansuda <thansuda_janthakham@cmu.ac.th>
2022-08-20 20:40:33 +0700

    fix get user id

--- src/main/java/com/se/shal/security/graphql/UserQueryQL.java
+++ src/main/java/com/se/shal/security/graphql/UserQueryQL.java
@@ -5,6 +5,7 @@ import com.se.shal.security.entity.UserDto;
 import com.se.shal.security.service.UserService;
 import com.se.shal.util.ShalMapper;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,8 +17,9 @@ public class UserQueryQL implements GraphQLQueryResolver {
     UserService userService;
 
     @Transactional
-    UserDto findByUserId(String userId) {
+    public UserDto findByUserId(String userId) {
         User user = userService.findByUserId(userId);
+        Hibernate.initialize(user.getAuthorities());
         return ShalMapper.INSTANCE.getUserDto(user);
     }
 }
