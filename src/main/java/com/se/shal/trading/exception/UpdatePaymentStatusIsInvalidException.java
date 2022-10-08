cb88825fb4f251ee559fe632a2fe96e2135d1e48
Thansuda <thansuda.j@orbitdigital.co.th>
2022-10-08 16:37:20 +0700

    add exception and mock data

---
+++ src/main/java/com/se/shal/trading/exception/UpdatePaymentStatusIsInvalidException.java
@@ -0,0 +1,39 @@
+package com.se.shal.trading.exception;
+
+import graphql.ErrorClassification;
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserNotExistException extends RuntimeException implements GraphQLError {
+
+
+    public UserNotExistException() {
+        super(String.format("No such user exists"));
+
+    }
+
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public ErrorClassification getErrorType() {
+        return ErrorType.ExecutionAborted;
+    }
+
+    @Override
+    public Map<String, Object> getExtensions() {
+        Map<String, Object> extension = new HashMap<>();
+
+        extension.put("error_code", 520);
+        extension.put("message", String.format("No such user exists"));
+        extension.put("displayMessage", String.format("No such user exists"));
+        return extension;
+    }
+}
